87379e5999925905e551a198b2649fb3e1307388
Rintu Raj.c <rinturajc180000@gmail.com>
2025-06-07 14:10:49 +0530

    Police removed

--- src/content.ts
+++ src/content.ts
@@ -5,31 +5,3 @@ import "tailwindcss/tailwind.css";
 import Overlay from "./components/overlay/Overlay.svelte";
 
 mount(Overlay, { target: document.body });
-
-Browser.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
-  capturePageData();
-
-  if (message.action === ACTION.OPEN_SIDEPANEL) {
-    await chrome.sidePanel.open({ tabId: message?.id || 0 });
-  }
-  if (message.action == ACTION.GET_CONTENT) {
-  }
-});
-
-if (window.location.protocol !== "chrome-extension:") {
-  capturePageData();
-}
-
-// Function to capture webpage data
-function capturePageData() {
-  if (window.location.protocol === "chrome-extension:") return;
-  const pageUrl = window.location.href;
-  const pageTitle = document.title;
-  const pageContent = document.body.innerText.substring(0, 500); // Limit preview
-  Browser.runtime.sendMessage({
-    action: ACTION.CAPTURE_CONTENT,
-    url: pageUrl,
-    title: pageTitle,
-    content: pageContent,
-  });
-}
